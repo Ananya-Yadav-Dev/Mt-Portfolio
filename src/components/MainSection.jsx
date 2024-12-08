1dacf0afb2e5cc2f341e0de0912faf238b51b73f
Ananya-Yadav-Dev <anniyadav2807@gmail.com>
2024-12-09 02:31:20 +0530

    Update MainSection.jsx

--- src/components/MainSection.jsx
+++ src/components/MainSection.jsx
@@ -13,7 +13,7 @@ const MainSection = () => {
           <p>
             I am a student currently studying in Thadomal Shahani Engineering College, Bandra West. I believe in the power of collaboration and continuous learning. I'm currently open to new opportunities and collaborations. If you're looking for a dedicated and forward-thinking web developer to bring your ideas to life, let's connect! Feel free to explore my portfolio, and don't hesitate to reach out for a chat about your next project.
           </p>
-          <button className="btn hire-btn">Hire Me</button>
+          <a href="#contactForm"><button className="btn hire-btn">Hire Me</button></a>
         </div>
         <div className="hero-image">
           <img src="./src/components/hero-img.png" alt="Ananya's hero" />
